728f799478101da7653cc069ffde11f8b1d955b7
liubumingbai123 <158013352+Liubumingbai@users.noreply.github.com>
2024-01-31 10:55:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-from flask import Flask, render_template, request
-import os
-import requests
+from flask import Flask,request,render_template
 import json
 import time
+import requests
 
-os.environ["REPLICATE_API_TOKEN"] = "r8_WDxiD8lcTepVxDJygMvfojLZBJK5Cr44EYT31"
+headers={
+    "Authorization" : "Token r8_WDxiD8lcTepVxDJygMvfojLZBJK5Cr44EYT31",
+    "Content-Type" : "application/json"
+}
+
 app = Flask(__name__)
-@app.route("/", methods=["GET","POST"])
+
+@app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
         q = request.form.get("q")
-        body = json.dumps({"version": "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf", "input": { "prompt": q } })
-        headers = {'Authorization': 'Token r8_WDxiD8lcTepVxDJygMvfojLZBJK5Cr44EYT31','Content-Type': 'application/json'}
-        output = requests.post('https://api.replicate.com/v1/predictions',data=body,headers=headers)
-        time.sleep(10)
-        get_url = output.json()['urls']['get']
-        print(get_url)
-        get_result = requests.post(get_url,headers=headers).json()['output']
-        print(get_result)
-        return(render_template("index.html", result=get_result[0]))
+        data = json.dumps(
+            {
+                "version" : "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",    
+                "input" : {"prompt":q}
+            }
+        )
+        r = requests.post('https://api.replicate.com/v1/predictions',data=data,headers=headers)
+        time.sleep(15)
+        r = r.json()["urls"]["get"]
+        r = requests.post(r,headers=headers).json()["output"]
+        return(render_template("index.html",r=r[0]))
     else:
-        return(render_template("index.html", result="waiting"))
+        return(render_template("index.html",r="Waiting for your entry"))
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     app.run()
